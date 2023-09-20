987c42dd548c8f3e8f600bf9d9d1c633a6cde49d
MarIb <martaa.ibarra@hotmail.com>
2023-09-20 19:15:34 +0200

    define and initialize variable

---
+++ 202309-w2ch3.js
@@ -0,0 +1,44 @@
+const songs = [
+  {
+    id: 1,
+    title: "Smooth Criminal",
+    artist: "Miachael Jackson",
+    genre: "pop",
+    duration: 258,
+    year: 1988,
+    ispeting: true,
+  },
+  {
+     id: 2,
+    title: "Wannabe",
+    artist:"Spice Girls",
+    genre: "pop",
+    duration: 243,
+    year: 1996,
+    ispeting: true,
+  },
+  {
+     id: 3,
+    title: "The Wrestler",
+    artist: "Bruce Springsteen",
+    genre: "rock",
+    duration: 231,
+    year: 2008,
+    ispeting: false,
+  },
+  {
+     id: 4,
+    title: "Christmas Tree Farm",  
+    artist: "Taylor Swift",
+    genre: "pop",
+    duration: 195,
+    year: 2019,
+    ispeting: no,
+  },
+]
+
+function addSong (song) = > {
+
+  
+}
+

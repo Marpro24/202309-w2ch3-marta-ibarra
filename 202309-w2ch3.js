492f68d01a9b69680dd55d784df5c0b32e95b4a2
MarIb <martaa.ibarra@hotmail.com>
2023-09-20 21:05:12 +0200

    Add find, indexof and splice functions to remove song

--- 202309-w2ch3.js
+++ 202309-w2ch3.js
@@ -9,16 +9,16 @@ const songs = [
     ispeting: true,
   },
   {
-     id: 2,
+    id: 2,
     title: "Wannabe",
-    artist:"Spice Girls",
+    artist: "Spice Girls",
     genre: "pop",
     duration: 243,
     year: 1996,
     ispeting: true,
   },
   {
-     id: 3,
+    id: 3,
     title: "The Wrestler",
     artist: "Bruce Springsteen",
     genre: "rock",
@@ -27,32 +27,38 @@ const songs = [
     ispeting: false,
   },
   {
-     id: 4,
-    title: "Christmas Tree Farm",  
+    id: 4,
+    title: "Christmas Tree Farm",
     artist: "Taylor Swift",
     genre: "pop",
     duration: 195,
     year: 2019,
-    ispeting: no,
+    ispeting: false,
   },
-]
+];
 
-const addNewSong = (newSong) => listSong.push(newSong);
+const addNewSong = (newSong) => listSongs.push(newSong);
 const newSong = {
-     id: 5,
-    title: "Paquito el chocolatero",  
-    artist: "Maricarmen y su trupa",
-    genre: "pop",
-    duration: 195,
-    year: 1995,
-    ispeting: no,
-
-
+  id: 5,
+  title: "Paquito el chocolatero",
+  artist: "Maricarmen y su tropa",
+  genre: "pop",
+  duration: 195,
+  year: 1995,
+  ispeting: false,
 };
 
 console.log(addNewSong(newSong));
 console.log(listSongs);
 
+const findSongTitle = (title) => {
+  const findSong = listSongs.find((song) => songs.title === title);
 
+  let findPosition = songs.indexOf(findSong);
 
-const removeSong = (removedSong)
+  songs.splice(findPosition, 1);
+
+  return songs;
+};
+
+console.log(findSongTitle("Wannabe"));
